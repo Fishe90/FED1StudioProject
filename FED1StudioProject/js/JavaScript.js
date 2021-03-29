e78d4793510648d837f679aa95f80a854ce68e20
Fishe90 <dillon.f617@gmail.com>
2021-03-28 22:53:45 -0500

    Created new Stylesheet + Added JS

---
+++ FED1StudioProject/js/JavaScript.js
@@ -0,0 +1,12 @@
+﻿window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("stickyNav")
+    } else {
+        navbar.classList.remove("stickyNav");
+    }
+}
